104d91f7258e07040911560d8330e46ffa7f7988
Igor Turchenko <iturch@gmail.com>
2015-02-12 14:50:58 +0300

    refactoring, p3

---
+++ app/src/main/java/ru/caseagency/twitteraddressbook/mainscreen/ContactsAdapter.java
@@ -0,0 +1,190 @@
+package ru.caseagency.twitteraddressbook.mainscreen;
+
+import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.v4.widget.CursorAdapter;
+import android.text.style.TextAppearanceSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AlphabetIndexer;
+import android.widget.QuickContactBadge;
+import android.widget.SectionIndexer;
+import android.widget.TextView;
+
+import ru.caseagency.twitteraddressbook.R;
+import ru.caseagency.twitteraddressbook.util.ImageLoader;
+
+/**
+ * This is a subclass of CursorAdapter that supports binding Cursor columns to a view layout.
+ * If those items are part of search results, the search string is marked by highlighting the
+ * query text. An {@link android.widget.AlphabetIndexer} is used to allow quicker navigation up and down the
+ * ListView.
+ */
+class ContactsAdapter extends CursorAdapter implements SectionIndexer {
+    private LayoutInflater mInflater; // Stores the layout inflater
+    private AlphabetIndexer mAlphabetIndexer; // Stores the AlphabetIndexer instance
+    private TextAppearanceSpan highlightTextSpan; // Stores the highlight text appearance style
+    private ImageLoader imageLoader;
+
+    /**
+     * Instantiates a new Contacts Adapter.
+     * @param context A context that has access to the app's layout.
+     */
+    public ContactsAdapter(Context context, Activity activity, ImageLoader imageLoader) {
+        super(context, null, 0);
+        this.imageLoader = imageLoader;
+
+        // Stores inflater for use later
+        mInflater = LayoutInflater.from(context);
+
+        // Loads a string containing the English alphabet. To fully localize the app, provide a
+        // strings.xml file in res/values-<x> directories, where <x> is a locale. In the file,
+        // define a string with android:name="alphabet" and contents set to all of the
+        // alphabetic characters in the language in their proper sort order, in upper case if
+        // applicable.
+        final String alphabet = context.getString(R.string.alphabet);
+
+        // Instantiates a new AlphabetIndexer bound to the column used to sort contact names.
+        // The cursor is left null, because it has not yet been retrieved.
+        mAlphabetIndexer = new AlphabetIndexer(null, ContactsQuery.SORT_KEY, alphabet);
+
+        // Defines a span for highlighting the part of a display name that matches the search
+        // string
+        highlightTextSpan = new TextAppearanceSpan(activity, R.style.searchTextHiglight);
+    }
+
+    /**
+     * Overrides newView() to inflate the list item views.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        // Inflates the list item layout.
+        final View itemLayout =
+                mInflater.inflate(R.layout.contact_list_item, viewGroup, false);
+
+        // Creates a new ViewHolder in which to store handles to each view resource. This
+        // allows bindView() to retrieve stored references instead of calling findViewById for
+        // each instance of the layout.
+        final ViewHolder holder = new ViewHolder();
+        holder.text1 = (TextView) itemLayout.findViewById(android.R.id.text1);
+        holder.text2 = (TextView) itemLayout.findViewById(android.R.id.text2);
+        holder.icon = (QuickContactBadge) itemLayout.findViewById(android.R.id.icon);
+
+        // Stores the resourceHolder instance in itemLayout. This makes resourceHolder
+        // available to bindView and other methods that receive a handle to the item view.
+        itemLayout.setTag(holder);
+
+        // Returns the item layout view
+        return itemLayout;
+    }
+
+    /**
+     * Binds data from the Cursor to the provided view.
+     */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        // Gets handles to individual view resources
+        final ViewHolder holder = (ViewHolder) view.getTag();
+
+        // For Android 3.0 and later, gets the thumbnail image Uri from the current Cursor row.
+        // For platforms earlier than 3.0, this isn't necessary, because the thumbnail is
+        // generated from the other fields in the row.
+        final String photoUri = cursor.getString(ContactsQuery.PHOTO_THUMBNAIL_DATA);
+
+        final String displayName = cursor.getString(ContactsQuery.DISPLAY_NAME);
+         {
+            // If the user didn't do a search, or the search string didn't match a display
+            // name, show the display name without highlighting
+            holder.text1.setText(displayName);
+
+            // If the search search is empty, hide the second line of text
+            holder.text2.setVisibility(View.GONE);
+        }
+
+        // Processes the QuickContactBadge. A QuickContactBadge first appears as a contact's
+        // thumbnail image with styling that indicates it can be touched for additional
+        // information. When the user clicks the image, the badge expands into a dialog box
+        // containing the contact's details and icons for the built-in apps that can handle
+        // each detail type.
+
+        // Generates the contact lookup Uri
+        final Uri contactUri = ContactsContract.Contacts.getLookupUri(
+                cursor.getLong(ContactsQuery.ID),
+                cursor.getString(ContactsQuery.LOOKUP_KEY));
+
+        // Binds the contact's lookup Uri to the QuickContactBadge
+        holder.icon.assignContactUri(contactUri);
+
+        // Loads the thumbnail image pointed to by photoUri into the QuickContactBadge in a
+        // background worker thread
+        imageLoader.loadImage(photoUri, holder.icon);
+    }
+
+    /**
+     * Overrides swapCursor to move the new Cursor into the AlphabetIndex as well as the
+     * CursorAdapter.
+     */
+    @Override
+    public Cursor swapCursor(Cursor newCursor) {
+        // Update the AlphabetIndexer with new cursor as well
+        mAlphabetIndexer.setCursor(newCursor);
+        return super.swapCursor(newCursor);
+    }
+
+    /**
+     * An override of getCount that simplifies accessing the Cursor. If the Cursor is null,
+     * getCount returns zero. As a result, no test for Cursor == null is needed.
+     */
+    @Override
+    public int getCount() {
+        if (getCursor() == null) {
+            return 0;
+        }
+        return super.getCount();
+    }
+
+    /**
+     * Defines the SectionIndexer.getSections() interface.
+     */
+    @Override
+    public Object[] getSections() {
+        return mAlphabetIndexer.getSections();
+    }
+
+    /**
+     * Defines the SectionIndexer.getPositionForSection() interface.
+     */
+    @Override
+    public int getPositionForSection(int i) {
+        if (getCursor() == null) {
+            return 0;
+        }
+        return mAlphabetIndexer.getPositionForSection(i);
+    }
+
+    /**
+     * Defines the SectionIndexer.getSectionForPosition() interface.
+     */
+    @Override
+    public int getSectionForPosition(int i) {
+        if (getCursor() == null) {
+            return 0;
+        }
+        return mAlphabetIndexer.getSectionForPosition(i);
+    }
+
+    /**
+     * A class that defines fields for each resource ID in the list item layout. This allows
+     * ContactsAdapter.newView() to store the IDs once, when it inflates the layout, instead of
+     * calling findViewById in each iteration of bindView.
+     */
+    private class ViewHolder {
+        TextView text1;
+        TextView text2;
+        QuickContactBadge icon;
+    }
+}
--- app/src/main/java/ru/caseagency/twitteraddressbook/mainscreen/ContactsListFragment.java
+++ app/src/main/java/ru/caseagency/twitteraddressbook/mainscreen/ContactsListFragment.java
@@ -17,8 +17,6 @@
 package ru.caseagency.twitteraddressbook.mainscreen;
 
 import android.app.Activity;
-import android.app.SearchManager;
-import android.content.Context;
 import android.content.res.AssetFileDescriptor;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -30,10 +28,6 @@ import android.support.v4.app.ListFragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
-import android.support.v4.widget.CursorAdapter;
-import android.text.SpannableString;
-import android.text.TextUtils;
-import android.text.style.TextAppearanceSpan;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.TypedValue;
@@ -42,15 +36,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
-import android.widget.AlphabetIndexer;
-import android.widget.QuickContactBadge;
-import android.widget.SectionIndexer;
-import android.widget.TextView;
 
 import java.io.FileDescriptor;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Locale;
 
 import ru.caseagency.twitteraddressbook.BuildConfig;
 import ru.caseagency.twitteraddressbook.R;
@@ -81,21 +70,12 @@ public class ContactsListFragment extends ListFragment implements
     // Defines a tag for identifying log entries
     private static final String TAG = "ContactsListFragment";
 
-    // Bundle key for saving previously selected search result item
-    private static final String STATE_PREVIOUSLY_SELECTED_KEY =
-            "com.example.android.contactslist.ui.SELECTED_ITEM";
-
     private ContactsAdapter mAdapter; // The main query adapter
     private ImageLoader mImageLoader; // Handles loading the contact image in a background thread
-    private String mSearchTerm; // Stores the current search query term
 
     // Contact selected listener that allows the activity holding this fragment to be notified of
     // a contact being selected
     private OnContactsInteractionListener mOnContactSelectedListener;
-
-    // Stores the previously selected search item so that on a configuration change the same item
-    // can be reselected again
-    private int mPreviouslySelectedSearchItem = 0;
 
     /**
      * Fragments require an empty constructor.
@@ -105,18 +85,6 @@ public class ContactsListFragment extends ListFragment implements
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Create the main contacts adapter
-        mAdapter = new ContactsAdapter(getActivity());
-
-        if (savedInstanceState != null) {
-            // If we're restoring state after this fragment was recreated then
-            // retrieve previous search term and previously selected search
-            // result.
-            mSearchTerm = savedInstanceState.getString(SearchManager.QUERY);
-            mPreviouslySelectedSearchItem =
-                    savedInstanceState.getInt(STATE_PREVIOUSLY_SELECTED_KEY, 0);
-        }
 
         /*
          * An ImageLoader object loads and resizes an image in the background and binds it to the
@@ -143,6 +111,9 @@ public class ContactsListFragment extends ListFragment implements
 
         // Add a cache to the image loader
         mImageLoader.addImageCache(getActivity().getSupportFragmentManager(), 0.1f);
+
+        // Create the main contacts adapter
+        mAdapter = new ContactsAdapter(getActivity(), getActivity(), mImageLoader);
     }
 
     @Override
@@ -175,10 +146,7 @@ public class ContactsListFragment extends ListFragment implements
             public void onScroll(AbsListView absListView, int i, int i1, int i2) {}
         });
 
-        // If there's a previously selected search item from a saved state then don't bother
-        // initializing the loader as it will be restarted later when the query is populated into
-        // the action bar search view (see onQueryTextChange() in onCreateOptionsMenu()).
-        if (mPreviouslySelectedSearchItem == 0) {
+        {
             // Initialize the loader, and create a loader identified by ContactsQuery.QUERY_ID
             getLoaderManager().initLoader(ContactsQuery.QUERY_ID, null, this);
         }
@@ -233,13 +201,6 @@ public class ContactsListFragment extends ListFragment implements
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        if (!TextUtils.isEmpty(mSearchTerm)) {
-            // Saves the current search string
-            outState.putString(SearchManager.QUERY, mSearchTerm);
-
-            // Saves the currently selected contact
-            outState.putInt(STATE_PREVIOUSLY_SELECTED_KEY, getListView().getCheckedItemPosition());
-        }
     }
 
     @Override
@@ -250,20 +211,7 @@ public class ContactsListFragment extends ListFragment implements
         if (id == ContactsQuery.QUERY_ID) {
             Uri contentUri;
 
-            // There are two types of searches, one which displays all contacts and
-            // one which filters contacts by a search query. If mSearchTerm is set
-            // then a search query has been entered and the latter should be used.
-
-            if (mSearchTerm == null) {
-                // Since there's no search string, use the content URI that searches the entire
-                // Contacts table
-                contentUri = ContactsQuery.CONTENT_URI;
-            } else {
-                // Since there's a search string, use the special content Uri that searches the
-                // Contacts table. The URI consists of a base Uri and the search string.
-                contentUri =
-                        Uri.withAppendedPath(ContactsQuery.FILTER_URI, Uri.encode(mSearchTerm));
-            }
+            contentUri = ContactsQuery.CONTENT_URI;
 
             // Returns a new CursorLoader for querying the Contacts table. No arguments are used
             // for the selection clause. The search string is either encoded onto the content URI,
@@ -399,219 +347,6 @@ public class ContactsListFragment extends ListFragment implements
 
         // If the decoding failed, returns null
         return null;
-    }
-
-    /**
-     * This is a subclass of CursorAdapter that supports binding Cursor columns to a view layout.
-     * If those items are part of search results, the search string is marked by highlighting the
-     * query text. An {@link AlphabetIndexer} is used to allow quicker navigation up and down the
-     * ListView.
-     */
-    private class ContactsAdapter extends CursorAdapter implements SectionIndexer {
-        private LayoutInflater mInflater; // Stores the layout inflater
-        private AlphabetIndexer mAlphabetIndexer; // Stores the AlphabetIndexer instance
-        private TextAppearanceSpan highlightTextSpan; // Stores the highlight text appearance style
-
-        /**
-         * Instantiates a new Contacts Adapter.
-         * @param context A context that has access to the app's layout.
-         */
-        public ContactsAdapter(Context context) {
-            super(context, null, 0);
-
-            // Stores inflater for use later
-            mInflater = LayoutInflater.from(context);
-
-            // Loads a string containing the English alphabet. To fully localize the app, provide a
-            // strings.xml file in res/values-<x> directories, where <x> is a locale. In the file,
-            // define a string with android:name="alphabet" and contents set to all of the
-            // alphabetic characters in the language in their proper sort order, in upper case if
-            // applicable.
-            final String alphabet = context.getString(R.string.alphabet);
-
-            // Instantiates a new AlphabetIndexer bound to the column used to sort contact names.
-            // The cursor is left null, because it has not yet been retrieved.
-            mAlphabetIndexer = new AlphabetIndexer(null, ContactsQuery.SORT_KEY, alphabet);
-
-            // Defines a span for highlighting the part of a display name that matches the search
-            // string
-            highlightTextSpan = new TextAppearanceSpan(getActivity(), R.style.searchTextHiglight);
-        }
-
-        /**
-         * Identifies the start of the search string in the display name column of a Cursor row.
-         * E.g. If displayName was "Adam" and search query (mSearchTerm) was "da" this would
-         * return 1.
-         *
-         * @param displayName The contact display name.
-         * @return The starting position of the search string in the display name, 0-based. The
-         * method returns -1 if the string is not found in the display name, or if the search
-         * string is empty or null.
-         */
-        private int indexOfSearchQuery(String displayName) {
-            if (!TextUtils.isEmpty(mSearchTerm)) {
-                return displayName.toLowerCase(Locale.getDefault()).indexOf(
-                        mSearchTerm.toLowerCase(Locale.getDefault()));
-            }
-            return -1;
-        }
-
-        /**
-         * Overrides newView() to inflate the list item views.
-         */
-        @Override
-        public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
-            // Inflates the list item layout.
-            final View itemLayout =
-                    mInflater.inflate(R.layout.contact_list_item, viewGroup, false);
-
-            // Creates a new ViewHolder in which to store handles to each view resource. This
-            // allows bindView() to retrieve stored references instead of calling findViewById for
-            // each instance of the layout.
-            final ViewHolder holder = new ViewHolder();
-            holder.text1 = (TextView) itemLayout.findViewById(android.R.id.text1);
-            holder.text2 = (TextView) itemLayout.findViewById(android.R.id.text2);
-            holder.icon = (QuickContactBadge) itemLayout.findViewById(android.R.id.icon);
-
-            // Stores the resourceHolder instance in itemLayout. This makes resourceHolder
-            // available to bindView and other methods that receive a handle to the item view.
-            itemLayout.setTag(holder);
-
-            // Returns the item layout view
-            return itemLayout;
-        }
-
-        /**
-         * Binds data from the Cursor to the provided view.
-         */
-        @Override
-        public void bindView(View view, Context context, Cursor cursor) {
-            // Gets handles to individual view resources
-            final ViewHolder holder = (ViewHolder) view.getTag();
-
-            // For Android 3.0 and later, gets the thumbnail image Uri from the current Cursor row.
-            // For platforms earlier than 3.0, this isn't necessary, because the thumbnail is
-            // generated from the other fields in the row.
-            final String photoUri = cursor.getString(ContactsQuery.PHOTO_THUMBNAIL_DATA);
-
-            final String displayName = cursor.getString(ContactsQuery.DISPLAY_NAME);
-
-            final int startIndex = indexOfSearchQuery(displayName);
-
-            if (startIndex == -1) {
-                // If the user didn't do a search, or the search string didn't match a display
-                // name, show the display name without highlighting
-                holder.text1.setText(displayName);
-
-                if (TextUtils.isEmpty(mSearchTerm)) {
-                    // If the search search is empty, hide the second line of text
-                    holder.text2.setVisibility(View.GONE);
-                } else {
-                    // Shows a second line of text that indicates the search string matched
-                    // something other than the display name
-                    holder.text2.setVisibility(View.VISIBLE);
-                }
-            } else {
-                // If the search string matched the display name, applies a SpannableString to
-                // highlight the search string with the displayed display name
-
-                // Wraps the display name in the SpannableString
-                final SpannableString highlightedName = new SpannableString(displayName);
-
-                // Sets the span to start at the starting point of the match and end at "length"
-                // characters beyond the starting point
-                highlightedName.setSpan(highlightTextSpan, startIndex,
-                        startIndex + mSearchTerm.length(), 0);
-
-                // Binds the SpannableString to the display name View object
-                holder.text1.setText(highlightedName);
-
-                // Since the search string matched the name, this hides the secondary message
-                holder.text2.setVisibility(View.GONE);
-            }
-
-            // Processes the QuickContactBadge. A QuickContactBadge first appears as a contact's
-            // thumbnail image with styling that indicates it can be touched for additional
-            // information. When the user clicks the image, the badge expands into a dialog box
-            // containing the contact's details and icons for the built-in apps that can handle
-            // each detail type.
-
-            // Generates the contact lookup Uri
-            final Uri contactUri = Contacts.getLookupUri(
-                    cursor.getLong(ContactsQuery.ID),
-                    cursor.getString(ContactsQuery.LOOKUP_KEY));
-
-            // Binds the contact's lookup Uri to the QuickContactBadge
-            holder.icon.assignContactUri(contactUri);
-
-            // Loads the thumbnail image pointed to by photoUri into the QuickContactBadge in a
-            // background worker thread
-            mImageLoader.loadImage(photoUri, holder.icon);
-        }
-
-        /**
-         * Overrides swapCursor to move the new Cursor into the AlphabetIndex as well as the
-         * CursorAdapter.
-         */
-        @Override
-        public Cursor swapCursor(Cursor newCursor) {
-            // Update the AlphabetIndexer with new cursor as well
-            mAlphabetIndexer.setCursor(newCursor);
-            return super.swapCursor(newCursor);
-        }
-
-        /**
-         * An override of getCount that simplifies accessing the Cursor. If the Cursor is null,
-         * getCount returns zero. As a result, no test for Cursor == null is needed.
-         */
-        @Override
-        public int getCount() {
-            if (getCursor() == null) {
-                return 0;
-            }
-            return super.getCount();
-        }
-
-        /**
-         * Defines the SectionIndexer.getSections() interface.
-         */
-        @Override
-        public Object[] getSections() {
-            return mAlphabetIndexer.getSections();
-        }
-
-        /**
-         * Defines the SectionIndexer.getPositionForSection() interface.
-         */
-        @Override
-        public int getPositionForSection(int i) {
-            if (getCursor() == null) {
-                return 0;
-            }
-            return mAlphabetIndexer.getPositionForSection(i);
-        }
-
-        /**
-         * Defines the SectionIndexer.getSectionForPosition() interface.
-         */
-        @Override
-        public int getSectionForPosition(int i) {
-            if (getCursor() == null) {
-                return 0;
-            }
-            return mAlphabetIndexer.getSectionForPosition(i);
-        }
-
-        /**
-         * A class that defines fields for each resource ID in the list item layout. This allows
-         * ContactsAdapter.newView() to store the IDs once, when it inflates the layout, instead of
-         * calling findViewById in each iteration of bindView.
-         */
-        private class ViewHolder {
-            TextView text1;
-            TextView text2;
-            QuickContactBadge icon;
-        }
     }
 
 }
